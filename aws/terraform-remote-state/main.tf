b2c50a36233a02356adac369540d94e5a77e8d21
Arunvel Sriram <arunvelsriram@gmail.com>
2019-07-28 14:21:13 +0530

    Add a comment for ensuring unique tf state path in s3

--- aws/terraform-remote-state/main.tf
+++ aws/terraform-remote-state/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "wex-terraform-state-1564297303"
-    key            = "terraform-remote-state/terraform.tfstate"
+    key            = "terraform-remote-state/terraform.tfstate" # this should be unique across terraform project directories
     region         = "us-east-2"
     dynamodb_table = "wex-terraform-state-lock"
   }
