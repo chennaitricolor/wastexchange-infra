8fda20a5aa04b2ada070a4e64b6bac6c1e6332f8
Arunvel Sriram <arunvelsriram@gmail.com>
2019-08-02 00:01:05 +0530

    Change terraform state key for iam

--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 terraform {
   backend "s3" {
     bucket         = "wex-terraform-state-1564297303"
-    key            = "iam-roles/terraform.tfstate" # this should be unique across terraform project directories
+    key            = "iam/terraform.tfstate" # this should be unique across terraform project directories
     region         = "us-east-2"
     dynamodb_table = "wex-terraform-state-lock"
   }
