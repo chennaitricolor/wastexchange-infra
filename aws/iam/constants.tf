c06c23490fa1ad1f0d201029b6fab6fe187aa10b
Arunvel Sriram <arunvelsriram@gmail.com>
2019-08-10 13:14:05 +0530

    Add local variables to main.tf

--- aws/iam/constants.tf
+++
@@ -1,8 +0,0 @@
-data "aws_caller_identity" "current" {}
-
-locals {
-  tf_state_bucket     = "wex-terraform-state-1564297303"
-  tf_state_lock_table = "wex-terraform-state-lock"
-  account_number      = "${data.aws_caller_identity.current.account_id}"
-}
-
--- aws/iam/main.tf
+++ aws/iam/main.tf
@@ -1,3 +1,11 @@
+data "aws_caller_identity" "current" {}
+
+locals {
+  tf_state_bucket     = "wex-terraform-state-1564297303"
+  tf_state_lock_table = "wex-terraform-state-lock"
+  account_number      = "${data.aws_caller_identity.current.account_id}"
+}
+
 provider "aws" {
   region  = "us-east-2"
   version = "~> 2.21"
